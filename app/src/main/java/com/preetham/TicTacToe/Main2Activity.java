beec0e0c0590aa2af69654420a0b1982aa009563
Preetham Reddy <bogadipreethamreddy@gmail.com>
2016-06-28 14:37:49 -0400

    Tictactoe game

---
+++ app/src/main/java/com/preetham/TicTacToe/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.preetham.TicTacToe;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class Main2Activity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
